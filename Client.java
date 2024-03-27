a0ad731bc79658cc7a8165ecadd6a82da4d01969
DH <120189749+asdfgasdfasdfg@users.noreply.github.com>
2024-03-27 09:27:19 +0900

    max, rpsgame

--- Client.java
+++ Client.java
@@ -1,2 +1,64 @@
-public class Client {
+package Client;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+class Client {
+    public static int inPort = 9999;
+    public static String address ="210.103.18.66";
+
+
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(address, inPort)) {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            ChatHandler c = new ChatHandler(socket);
+            Thread myThread = new Thread(c);
+            myThread.start();
+            Scanner sc = new Scanner(System.in);
+            String line = null;
+            while (!"exit".equalsIgnoreCase(line)) {
+                line = sc.nextLine();
+                out.println(line);
+                out.flush();
+            }
+            sc.close();
+        }
+        catch (Exception e) {}
+    }
+
 }
+
+
+class ChatHandler implements Runnable {
+    private final Socket socket;;
+
+    public ChatHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println("("+ socket.getInetAddress()+ ") " + line); //
+            }
+        }
+        catch (IOException e) { }
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                    socket.close();
+                }
+            }
+            catch (IOException e) { }
+        }
+    }
+}
+
--- Server.java
+++ Server.java
@@ -1,11 +1,24 @@
+package Server;
+
 import java.io.*;
 import java.net.*;
 import java.util.Vector;
 
 
+
 class Server {
     public static int inPort = 9999;
     public static Vector<Client> clients = new Vector<Client>();
+
+    final double rock = 0;
+    final double paper = 1;
+    final double scissors = 2;
+    
+    int rcount = 0;
+    int pcount = 0;
+    int scount = 0;
+    int selectedClients = 0;
+    double result;
 
     public static void main(String[] args) throws Exception {
         new Server().createServer();
@@ -30,7 +43,10 @@ class Server {
         Socket socket;
         PrintWriter out = null;
         BufferedReader in = null;
-
+        boolean r = false;
+        boolean p = false;
+        boolean s = false;
+        
         public Client(Socket socket) throws Exception {
             this.socket = socket;
             out = new PrintWriter(socket.getOutputStream(), true);
@@ -42,6 +58,101 @@ class Server {
             out.println(msg);
         }
 
+        public String rpsNumToString(double num) {
+        	if (num == rock) {
+        		resetSelectionRps();
+        		r = true;
+        		rcount++;
+        		return "주먹";
+        	} else if (num == paper) {
+        		resetSelectionRps();
+        		p = true;
+        		pcount++;
+        		return "보자기";
+        	} else if (num == scissors) {
+        		resetSelectionRps();
+        		s = true;
+        		scount++;
+        		return "가위";
+        	} else {
+        		return "입력 오류";
+        	}
+        }
+  
+        public void rpsPlay(boolean r, boolean p, boolean s) {
+        	if (rcount == 0) {
+        		if (pcount != 0 && scount != 0) {
+        			if (p == true) {
+        				result = -1;
+        			} else {
+        				result = 1;
+        			}
+        		} else { // 1종류만 나오면 무승부.
+        			result = 0;
+        		}
+            } else if (pcount == 0) {
+        		if (scount != 0 && rcount != 0) {
+        			if (s == true) {
+        				result = -1;
+        			} else {
+        				result = 1;
+        			}
+        		} else { // 1종류만 나오면 무승부.
+        			result = 0;
+        		}            				
+        	} else if (scount == 0) {
+        		if (rcount != 0 && pcount != 0) {
+        			if (r == true) {
+        				result = -1;
+        			} else {
+        				result = 1;
+        			}
+        		} else { // 1종류만 나오면 무승부.
+        			result = 0;
+        		}        		
+        	} else { // 셋 다 카운트되면 무승부.
+        		result = 0;
+        	}
+        }
+        
+        public synchronized void rpsResult() {
+        	Vector<String> clientStr = new Vector<String>();
+        	
+        	int i = 0;
+    		for (Client c : clients) {
+                if (result == 1) {
+                    clientStr.add("승리");
+                } else if (result == -1) {
+                	clientStr.add("패배");
+                } else {
+                	clientStr.add("무승부");
+                }
+            }
+    		
+    		
+    		
+        	if (selectedClients == clients.size()) {
+        		for (int j = 0; j < clients.size(); j++) {
+        		    // 각 클라이언트에 대해 해당하는 결과를 전송합니다.
+        			clients.get(j).send(clientStr.elementAt(j));
+        		}
+            	
+        		selectedClients = 0;
+        		rcount = 0;
+                pcount = 0;
+                scount = 0;
+                result = 0;
+        	}
+        	
+        }
+     
+        public void resetSelectionRps() {
+            r = false;
+            p = false;
+            s = false;
+    		selectedClients++;
+        }
+
         @Override
         public void run() {
             String line;
@@ -49,7 +160,10 @@ class Server {
                 while(true) {
                     line = in.readLine();
                     System.out.println("("+ socket.getInetAddress()+ ") " + line);
-                    sendtoall(line);
+                    double num = Double.parseDouble(line);
+                    this.send("당신의 선택: " + rpsNumToString(num) + "!");
+                    rpsPlay(r, p, s);
+                    rpsResult();
                 }
             }
             catch (IOException e) { }
@@ -65,6 +179,9 @@ class Server {
                 catch (IOException e) { }
             }
         }
+      
+        
+        
 
     }
 
